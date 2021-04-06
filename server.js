4f53fbd2537368398ac3376ac0167bd8404fd25c
Amjad Mesmar <Amjad.Mesmar@gmail.com>
2021-04-07 02:07:18 +0300

    database added and working with API

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
+/* eslint-disable quotes */
 /* eslint-disable camelcase */
 /* eslint-disable no-redeclare */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-//DOTENV (read our enviroment variable)
-require('dotenv').config();
 
+//DOTENV (read our enviroment variable)
+
+require('dotenv').config();
 // Application Dependencies
+
 const express = require('express');
-//CORS = Cross Origin Resource Sharing
-const cors = require('cors');
-// client-side HTTP request library
-const superagent = require('superagent');
+const pg = require('pg');
+const cors = require('cors'); //CORS = Cross Origin Resource Sharing
+const superagent = require('superagent'); // client-side HTTP request library
+
 
 // Application Setup
 const PORT = process.env.PORT || 3000;
+
+const client = new pg.Client(process.env.DATABASE_URL); // adding DATABASE URL
+// clients.connect(); // Activate the client
+// const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
+
+
 const app = express();
 app.use(cors());
 
@@ -21,9 +30,10 @@ app.use(cors());
 // Routes
 app.get('/', homeRouteHandler);
 app.get('/location', locationHandler);
+app.get('/showlocation', locationTableHandler);
 app.get('/weather', weatherHandler);
 app.get('/parks',parksHandler);
-app.get('*', notFoundHandler);
+app.get('*', notFoundHandler); //error handler
 
 
 
@@ -33,37 +43,67 @@ function homeRouteHandler(request, response) {
 }
 
 //http://localhost:3000/location?city=amman
+
 function locationHandler(req, res) {
   // get data from api server (locationIQ)
   // send a request using superagent library (request url + key)
   // console.log(req.query);
+
   let cityName = req.query.city;
-  // console.log(cityName);
+  // let SQL = `SELECT * FROM location;`;
   let key = process.env.LOCATION_KEY;
   let LocURL = `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${cityName}&format=json`;
 
   console.log('before superagent');
+
   superagent.get(LocURL) //send request to LocationIQ API
     .then(geoData => {
       console.log('inside superagent');
-      // console.log(geoData.body);
       let gData = geoData.body;
       const locationData = new Location(cityName, gData);
-      res.send(locationData);
-      // return locationData;
+      let SQL = `SELECT * FROM locations WHERE search_query=$1`;
+      let cityValue = [cityName];
+      client.query(SQL, cityValue).then(result => {
+        if (result.rowCount) {
+          res.send(result.rows[0]);
+        }
+        else {
+          let search_query= locationData.search_query;
+          let formatted_query = locationData.formatted_query;
+          let latitude = locationData.latitude;
+          let longitude = locationData.longitude;
+          SQL = `INSERT INTO locations (search_query,formatted_query,latitude,longitude) VALUES ($1,$2,$3,$4) RETURNING *;`;
+          let safeValues = [search_query,formatted_query,latitude,longitude];
+          client.query(SQL,safeValues)
+            .then(result=>{
+              res.send(result.rows[0]);
+            });
+        }
+      });
+      // }
+      // res.send(locationData);
     })
-  // .then((locationData) => {
-  //     res.send(locationData);
-  // })
     .catch(error => {
-      console.log('inside superagent');
       console.log('Error in getting data from LocationIQ server');
       console.error(error);
       res.send(error);
     });
+
+
   console.log('after superagent');
 
 }
+function locationTableHandler (req,res){
+  let SQL = `SELECT * FROM locations;`;
+  client.query(SQL)
+    .then (result=>{
+      res.send(result.rows);
+    })
+    .catch(error=>{
+      res.send(error);
+    });
+}
+
 //https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh,NC&key=API_KEY
 
 function weatherHandler(req, res) {
@@ -139,6 +179,7 @@ function Location (city, locData){
   this.longitude = locData[0].lon;
 }
 
+
 function Weather (weatherData){
   this.forecast = weatherData.weather.description;
   this.time = weatherData.valid_date;
@@ -165,7 +206,15 @@ function Park (parksData) {
 // });
 
 
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`listening on port ${PORT}`);
+// });
+
+client.connect()
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`listening on ${PORT}`)
+    );
+  });
+
 
