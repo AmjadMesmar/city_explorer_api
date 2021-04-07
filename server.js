009fe77d07af21ca07df9499326694cd2dcb56ca
Amjad Mesmar <Amjad.Mesmar@gmail.com>
2021-04-08 02:47:27 +0300

    added movies and yelp APIs

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ require('dotenv').config();
 // Application Dependencies
 
 const express = require('express');
-const pg = require('pg');
+const pg = require('pg'); // install pg package:  npm i pg
 const cors = require('cors'); //CORS = Cross Origin Resource Sharing
 const superagent = require('superagent'); // client-side HTTP request library
-
+const yelp = require( 'yelp-fusion' ); // This is needed for yelp and yelp-fusion to work!
 
 // Application Setup
 const PORT = process.env.PORT || 3000;
@@ -22,20 +22,18 @@ const PORT = process.env.PORT || 3000;
 // clients.connect(); // Activate the client
 const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
 
-
 const app = express();
 app.use(cors());
-
 
 // Routes
 app.get('/', homeRouteHandler);
 app.get('/location', locationHandler);
 app.get('/showlocation', locationTableHandler);
 app.get('/weather', weatherHandler);
+app.get('/movies', moviesHandler);
 app.get('/parks',parksHandler);
+app.get('/yelp',yelpPagingHandler);
 app.get('*', notFoundHandler); //error handler
-
-
 
 // Routes Handlers
 function homeRouteHandler(request, response) {
@@ -50,7 +48,6 @@ function locationHandler(req, res) {
   // console.log(req.query);
 
   let cityName = req.query.city;
-  // let SQL = `SELECT * FROM location;`;
   let key = process.env.LOCATION_KEY;
   let LocURL = `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${cityName}&format=json`;
 
@@ -64,6 +61,7 @@ function locationHandler(req, res) {
       let SQL = `SELECT * FROM locations WHERE search_query=$1`;
       let cityValue = [cityName];
       client.query(SQL, cityValue).then(result => {
+        // console.log(result);
         if (result.rowCount) {
           res.send(result.rows[0]);
         }
@@ -166,6 +164,73 @@ function parksHandler(req, res) {
   console.log('after superagent');
 }
 
+function moviesHandler(req, res) {
+  let moviesArray =[];
+  // let cityName = req.query.query;
+  let moviesKey = process.env.MOVIES_KEY;
+  let moviesURL = `https://api.themoviedb.org/3/discover/movie?api_key=${moviesKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false`;
+  superagent.get(moviesURL)
+    .then(geoData => {
+      let geoD = geoData.body.results;
+      console.log('inside superagent');
+      // console.log(geoD);
+      geoD.map (movieValue => {
+      //   for (let i in geoData){
+        const moviesData = new Movie(movieValue);
+        moviesArray.push(moviesData);
+        // console.log(moviesData.image_url);
+
+      });
+      // console.log('movies array',moviesArray);
+      res.send(moviesArray);
+
+    })
+
+    .catch(error => {
+      console.log('inside superagent');
+      console.log('Error in getting data from www.themoviedb.org server');
+      console.error(error);
+      res.send(error);
+    });
+  console.log('after superagent');
+}
+
+// to activate yelp run npm install yelp-fusion and npm i yelp.
+let pageCounter =1;
+function yelpPagingHandler( req, res ) {
+  let yelpArr = [];
+  pageCounter++;
+  let cityName = req.query.search_query;
+  const yelpKey = process.env.YELP_FUSION_KEY;
+  const searchQuery = {};
+  let displayStartingPage = 1;
+
+  function getYelp( city,pageCounter ){
+    const displayPage = 5;
+    const displayStartingPage = ( ( pageCounter -1 ) * displayPage + 1 );
+    searchQuery.location = city;
+    searchQuery.limit = displayPage;
+    searchQuery.offset = displayStartingPage;
+    pageCounter++;
+  }
+  getYelp( cityName,pageCounter );
+  const client = yelp.client( yelpKey );
+  client.search( searchQuery )
+    .then( ( yelpGeoData ) => {
+      console.log(yelpGeoData);
+      let yelpData = yelpGeoData.jsonBody.businesses;
+      yelpArr = yelpData.map( ( yelpData ) => {
+        return new Yelp( yelpData );
+      } );
+      console.log(yelpArr);
+      res.send( yelpArr );
+    } )
+    .catch( ( error ) => {
+      res.send( error );
+    } );
+}
+
+
 function notFoundHandler(req, res) {
   res.status(404).send('Error, the page you are looking for does not exist :(');
 }
@@ -194,6 +259,24 @@ function Park (parksData) {
   this.fee = parksData.fees;
   this.description = parksData.description;
   this.url = parksData.url;
+}
+
+function Movie (movieData){
+  this.title = movieData.original_title;
+  this.overview = movieData.overview;
+  this.average_votes = movieData.vote_average;
+  this.total_votes = movieData.vote_count;
+  this.image_url = `https://image.tmdb.org/t/p/w500${movieData.poster_path}`;
+  this.popularity = movieData.popularity;
+  this.released_on = movieData.release_date;
+}
+
+function Yelp( yData ) {
+  this.name = yData.name;
+  this.image_url = yData.image_url;
+  this.price = yData.price;
+  this.rating = yData.rating;
+  this.url = yData.url;
 }
 // // this have to be at the end so it will check if nothing above was the route:
 
